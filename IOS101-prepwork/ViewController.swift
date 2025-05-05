f4241630e3ed6ba96dbb349d0598520a8999b11f
smuktha29 <smuktha29@gmail.com>
2025-05-04 22:54:07 -0400

    Add files via upload

---
+++ IOS101-prepwork/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  IOS101-prepwork
+//
+//  Created by Sathvikasri Muktha on 5/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
